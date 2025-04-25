02fd6f72c9ecbb585bf7e86a3c5c149dc4a8536c
amritha-11 <151115364+amritha-11@users.noreply.github.com>
2025-04-25 11:07:46 +0530

    Add files via upload
    second commit

---
+++ src/main/test/java/org/test/WebpageTest.java
@@ -0,0 +1,35 @@
+package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import static org.testng.Assert.assertTrue;
+public class WebpageTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://amritha-11.github.io/exp2/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        System.out.println("Page Title: " + actualTitle);
+        String expectedTitle = "My Portfolio";
+        Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
+        Assert.assertTrue(actualTitle.contains("Portfolio"), "Title should contain 'Design'");
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+}
